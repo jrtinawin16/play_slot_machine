61cbaee66758b831a1045c639c54d27da9674293
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2025-01-25 00:00:14 +0800

    Added conditiosn for prizes

--- play_slot_machine_in_python.py
+++ play_slot_machine_in_python.py
@@ -6,8 +6,23 @@ def spin_row():
 def print_row(row):
     print(" | ".join(row))
 
-def get_payout():
-    pass
+def get_payout(row, bet):
+    if row [0] == row [1] == row[2]:
+        if row[0] == "🍒":
+            return bet - 100
+        elif row[0] == "🍉":
+            return bet + 100
+        elif row[0] == "🍋":
+            return bet * 2
+        elif row[0] == "🍓":
+            return bet * 3
+        elif row[0] == "🍀":
+            return bet * 5
+        elif row[0] == "🔔":
+            return bet * 10
+        elif row[0] == "⭐":
+            return bet * 20
+    return 0
 
 def main():
     balance = 1000
