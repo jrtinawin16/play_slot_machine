fa7a7c904209a0411d81f401dfe57a9fe12230e4
John Reydo Tinawin <johnreydo.tinawin@gmail.com>
2025-01-24 23:37:17 +0800

    Added prompt to place bet and conditions to subtract it to balance

--- play_slot_machine_in_python.py
+++ play_slot_machine_in_python.py
@@ -8,11 +8,27 @@ def get_payout():
     pass
 
 def main():
-    balance = 100
+    balance = 1000
     
     print("*******************************")
     print("Welcome to Reydo's Lucky Slots!")
     print("    🍒 🍉 🍋 🍀 🍓 🔔 ⭐    ")
     print("*******************************")
+
+    while balance > 0: 
+        print(f"Current balance: ₱{balance}")
+        bet = input("Place your bet amount: ₱")
+        if not bet.isdigit():
+            print("Invalid amount, please try again.")
+            continue
+
+        bet = int(bet)
+        if bet > balance:
+            print("Insufficient money to bet.")
+            continue
+        if bet <= 0:
+            print("Bet must be greater than 0.")
+            continue
+        balance -= bet
 if __name__ == '__main__':
     main()
